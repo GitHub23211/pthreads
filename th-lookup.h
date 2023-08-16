3d0240db55eec510c9f2932eac4f6db45b14650b
Joshua Santos <joshua.santos1@students.mq.edu.au>
2023-08-17 03:26:57 +1000

    added func defs

--- th-lookup.h
+++ th-lookup.h
@@ -4,7 +4,11 @@
 
 void* request(void* input);
 void* resolve(void* output);
-void tsafe_queue_push(queue* q, char* buffer);
+void tsafe_queue_push(queue* q, void* p);
 char* tsafe_queue_pop(queue* q);
 void tsafe_write(FILE* output, char* hostname, char* ip);
 void tsafe_increment();
+void tsafe_add_pusher();
+void tsafe_decerement_pusher();
+int tsafe_queue_full(queue* q);
+int tsafe_queue_empty(queue* q);
